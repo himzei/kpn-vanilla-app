0bf795c4f4c769dbcd50b7a5280a2e46e45796d7
조현일 <himzei@gmail.com>
2022-09-02 08:35:52 +0900

    20220902

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,3 @@
-
 // search input 이이콘 클릭시 placeholde
 const searchEl = document.querySelector(".search");
 const searchInputEl = searchEl.querySelector("input");
@@ -14,7 +13,7 @@ searchInputEl.addEventListener("focus", function () {
 searchInputEl.addEventListener("blur", function () {
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
-  searchInputEl.value = ""
+  searchInputEl.value = "";
 });
 
 // Notice Swiper JS
@@ -22,6 +21,18 @@ new Swiper(".notice-line .swiper", {
   direction: "vertical",
   autoplay: true,
   loop: true,
+});
+
+// Section Links
+new Swiper(".links .swiper", {
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl: ".links .swiper-prev",
+    nextEl: ".links .swiper-next",
+  },
 });
 
 // Swiper Promotion
@@ -40,3 +51,39 @@ new Swiper(".promotion .swiper", {
     nextEl: ".promotion .swiper-next",
   },
 });
+
+// Section : Contact
+// 다음 지도 띄우기
+var container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
+var options = {
+  //지도를 생성할 때 필요한 기본 옵션
+  center: new kakao.maps.LatLng(35.87562431932182, 128.68133577245086), //지도의 중심좌표.
+  level: 3, //지도의 레벨(확대, 축소 정도)
+};
+
+var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+// 마커가 표시될 위치입니다
+var markerPosition = new kakao.maps.LatLng(
+  35.87562431932182,
+  128.68133577245086
+);
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+  position: markerPosition,
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
+
+// 개인정보 취금 텍스트에어리어
+const checkedContents = `
+Ken's Programming Note(이하 ‘KPN’라 한다)는 개인정보 보호법 제30조에 따라 정보 주체의 개인정보를 보호하고 이와 관련한 고충을 신속하고 원활하게 처리할 수 있도록 하기 위하여 다음과 같이 개인정보 처리지침을 수립, 공개합니다.
+
+제1조 (개인정보의 처리목적)
+
+`;
+const usercheck = document.querySelector("#usercheck");
+
+usercheck.value = checkedContents;
